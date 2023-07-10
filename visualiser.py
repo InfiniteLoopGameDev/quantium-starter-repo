e41068905f290d1e86200ec15a69528902ef5e4b
InfiniteLoop <44796943+InfiniteLoopGameDev@users.noreply.github.com>
2023-07-10 13:16:13 +0100

    Region Selector
    Wow pretty!

--- visualiser.py
+++ visualiser.py
@@ -1,20 +1,65 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
 
-data_file = pd.read_csv('data/pink_morsel.csv')
+data = pd.read_csv('data/pink_morsel.csv')
+data = data.sort_values(by="date")
 
-fig = px.line(data_file, x='date', y='sales', color='region')
+fig = px.line(data, x='date', y='sales', color='region')
 
-app.layout = html.Div([
-    html.H1('Pink Morsel Sales', style={
+graph = dcc.Graph(
+    figure=fig,
+    style={
+        'width' : '80vw',
+        'height' : '80vh',
+        'display': 'block',
+        'margin': 'auto',
+    },
+    id='graph')
+
+header = html.H1('Pink Morsel Sales', style={
         'textAlign': 'center',
         'fontFamily': 'sans-serif'
-    }),
-    dcc.Graph(figure=fig)
+})
+
+radio = dcc.RadioItems(
+    ["North", "East", "South", "West", "All"],
+    "All",
+    id="radio",
+    inline=True,
+    labelStyle={
+        'padding': '1em',
+    },
+    style={
+        'textAlign': 'center',
+        'fontFamily': 'sans-serif',
+        'display': 'block',
+    }
+)
+
+app.layout = html.Div([
+    header,
+    graph,
+    radio
 ])
+
+@callback(
+    Output('graph', 'figure'),
+    Input('radio', 'value'))
+def update_graph(selected):
+    selected = [selected.lower()]
+    if selected == ["all"]:
+        selected = ["north", "east", "south", "west"]
+    filtered_data = data[data.region.isin(selected)]
+    fig = px.line(filtered_data, x='date', y='sales', color='region', labels={
+    'date': 'Date',
+    'sales': 'Sales ($)',
+    'region': 'Region',
+})
+    fig.update_layout()
+    return fig
                       
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
